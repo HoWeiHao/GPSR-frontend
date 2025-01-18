a545a33953f1ad7adf24fb350d186493b60c176f
HoWeiHao <66128067+HoWeiHao@users.noreply.github.com>
2025-01-18 18:34:26 +0800

    Added banner

---
+++ my-app/components/Banner.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Banner.css';
+
+const Banner: React.FC = () => {
+  return (
+    <div className="banner">
+      <h1>GSPR App</h1>
+    </div>
+  );
+};
+
+export default Banner;
